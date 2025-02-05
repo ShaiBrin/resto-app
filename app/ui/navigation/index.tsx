1ddb36097cad4c9b0e5051201562a1c7399c3c59
ShaiBrin <Ishr_Shai@live.ca>
2025-02-04 21:56:53 -0500

    feat: Navbar with the menu, catering, ship contact ahd home has been added to the home page.
    A video is on the homepage

---
+++ app/ui/navigation/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useState } from "react";
+import links from "../../../data/links.json"
+import {  Links } from "@/app/types/";
+import Navbar from "./navbar";
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Navbar links={links as Links}  isOpen={isOpen} toggle={toggle} />
+    </>
+  );
+};
+
+export default Navigation;
---
+++ app/ui/navigation/navbar/index.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import { Links } from "@/app/types";
+import NavItem from "../navItems";
+
+interface NavBarProps {
+  links: Links;
+  isOpen: boolean;
+  toggle: () => void;
+}
+
+const Navbar: React.FC<NavBarProps> = ({ links, isOpen, toggle }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={`fixed w-full top-0 left-0 z-20 transition-opacity duration-300 ${
+        isOpen ? "opacity-0 pointer-events-none" : "opacity-100"
+      }`}
+      style={{
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+      }}
+    >
+      <div className="w-full h-20 text-black font-sans">
+        <div className="w-full px-6 h-full flex justify-between items-center">
+          {/* Home Button */}
+          <Link href="/">
+            <HomeIcon className="text-gray-500 hover:text-black transition-colors duration-300 text-3xl" />
+          </Link>
+
+          {/* Nav Links (Desktop) */}
+          <ul className="hidden md:flex w-full items-center">
+            <div className="flex justify-center w-full gap-x-16">
+                {links.links
+                .filter((link) => link !== "/contact")
+                .map((link) => (
+                    <NavItem key={link} link={link} />
+                ))}
+            </div>
+            <NavItem link="/contact" />
+            </ul>
+
+          {/* Hamburger Menu (Mobile) */}
+          {isMobile && (
+            <MenuIcon
+              className="md:hidden cursor-pointer text-gray-600"
+              fontSize="large"
+              onClick={toggle}
+            />
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
