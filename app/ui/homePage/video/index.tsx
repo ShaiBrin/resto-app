41b993450fa04b658363a392f9bac6803fefeb6e
ShaiBrin <Ishr_Shai@live.ca>
2025-02-16 14:24:30 -0500

    refac: Changed to a bbq video for the homepage

--- app/ui/homePage/video/index.tsx
+++ app/ui/homePage/video/index.tsx
@@ -10,7 +10,7 @@ const VideoContent = () => {
         muted
         playsInline
       >
-        <source src="/video.mp4" type="video/mp4" />
+        <source src="/videoBBQ.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/30">
